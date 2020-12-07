0f786c592e83d5a2386c2d3758f9cd7f5aaad193
zhangquanshuo <zhangquanshuo@yssrech.com>
2020-12-07 18:00:42 +0800

    第一次上传

---
+++ src/main/java/com/xsh/util/Test.java
@@ -0,0 +1,15 @@
+package com.xsh.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhangzhihua
+ * @date 2020/12/7 10:01
+ */
+public class Test {
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<String>();
+        a.forEach(p-> System.out.println(p));
+    }
+}
